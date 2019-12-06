f9aa7e82d3b4f6ded71a66d460459ee96028b76b
kimlie0401 <54561489+kimlie0401@users.noreply.github.com>
2019-12-05 17:46:41 -0800

    Home Container

--- src/api.js
+++ src/api.js
@@ -8,16 +8,6 @@ const api = axios.create({
   }
 });
 
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "https://api.themoviedb.org/3/",
-  params: {
-    api_key: "10923b261ba94d897ac6b81148314a3f",
-    language: "en-US"
-  }
-});
-
 export const moviesApi = {
   nowPlaying: () => api.get("movie/now_playing"),
   upcoming: () => api.get("movie/upcoming"),
